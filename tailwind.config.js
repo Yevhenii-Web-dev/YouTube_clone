8e323bd9f1d5e43bb846e751685805848490da34
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2022-12-12 17:09:17 +0100

    init + header

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      borderRadius: {
+        "4xl": "40px",
+      },
+    },
+  },
+  plugins: [],
+};
